4b3a8912f8d59ddc831cc48f2ef57e7353b61cb1
AKukushkin <kukpracticum@yandex.by>
2026-08-10 19:19:29 +0000

    answer_fixs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log("!!! СКРИПТ INDEX.JS НАЧАЛ ВЫПОЛНЕНИЕ !!!");
 import '../src/css/style.scss';
 import Handlebars from "handlebars";
 
@@ -9,6 +8,7 @@ import settingsTemplate from "./handlebars/components/settings/settings.hbs?raw"
 import settingsEditTemplate from "./handlebars/components/settings/edit/user.hbs?raw";
 import settingsPasswordTemplate from "./handlebars/components/settings/edit/password.hbs?raw"
 import error404Template from "./handlebars/components/404.hbs?raw";
+import error500Template from "./handlebars/components/500.hbs?raw";
 import { mockData } from "./mocks"; 
 
 //Роуты посоветовал сделать ИИ для корректной работы в netlify
@@ -23,9 +23,7 @@ const routes = {
 };
 
 function render() {
-    console.log("!!! Функция render() успешно вызвана !!!");
     let path = window.location.pathname;
-    console.log("Текущий путь в браузере:", path);
 
     // Нормализация путей для Netlify
     // Если хостинг дописывает /index.html или в конце пути стоит лишний слэш (например, /chats/), очищаем их
@@ -40,22 +38,25 @@ function render() {
 
     // Ищем шаблон. Если роут не найден, принудительно берем error404Template
     const template = routes[path] || error404Template;
-     console.log("Найденный шаблон:", template ? "Да (строка существует)" : "Нет (пусто!)");
 
     try {
         // Компиляция шаблона Handlebars
         const compiledTemplate = Handlebars.compile(template);
-        console.log("Шаблон Handlebars успешно скомпилирован!");
         
         // Рендерим HTML в тег body
         document.body.innerHTML = compiledTemplate(mockData);
-        console.log("HTML успешно записан в body. Текущий body:", document.body.innerHTML);
 
         // Навешиваем обработчики событий на новые элементы
         initEventListeners();
     } catch (error) {
         // Если Handlebars упадет, мы ХОТЯ БЫ увидим ошибку в консоли Netlify
         console.error("Критическая ошибка компиляции Handlebars:", error);
+
+        const compiled500 = Handlebars.compile(error500Template);
+        document.body.innerHTML = compiled500(mockData);
+        
+        // Заново инициализируем ссылки, чтобы с 500 страницы можно было уйти назад/на главную
+        initEventListeners();
     }
 }
 
